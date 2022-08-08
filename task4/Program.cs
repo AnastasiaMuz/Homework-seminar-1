a4154c4f7074ab0a2171bc3e73798858086d55a6
«Anastasia» <muzalkova.anastasia@gmail.com>
2022-08-08 08:40:26 +0300

    add task4

---
+++ task4/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine ("Введите первое число: ");
+int numberA = Convert.ToInt32 (Console.ReadLine ());
+Console.WriteLine ("Введите второе число: ");
+int numberB = Convert.ToInt32 (Console.ReadLine ());
+Console.WriteLine ("Введите третье число: ");
+int numberС = Convert.ToInt32 (Console.ReadLine ());
+
+int max = numberA;
+if (numberA > numberB)
+{
+    max = numberA;
+}
+else
+{
+    max = numberB;
+}
+if (numberС > max)
+{
+    max = numberС;
+}
+
+Console.WriteLine (max + " максимальное");
