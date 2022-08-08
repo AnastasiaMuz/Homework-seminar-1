be84b94e23fbcc4893813235e8c7663917d8eb70
AnastasiaMuz <109463676+AnastasiaMuz@users.noreply.github.com>
2022-08-08 09:20:03 +0300

    Add files via upload

--- task2/Program.cs
+++ task2/Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿Console.WriteLine ("Введите первое число: ");
+int numberA = Convert.ToInt32 (Console.ReadLine ());
+Console.WriteLine ("Введите второе число: ");
+int numberB = Convert.ToInt32 (Console.ReadLine ());
+
+if (numberA > numberB)
+{
+    Console.WriteLine (numberA + " большее, " + numberB + " меньшее");
+}
+else
+{
+    Console.WriteLine (numberB + " большее, " + numberA + " меньшее");
+}
